34c53e99893b36be93ed69dfc964a5ba6d79b8a5
eightharsh <interact.harsh@gmail.com>
2025-04-05 19:25:56 +0530

    speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { Navbar } from "../components/navbar"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
